34d6e0a4c3fcf8c1c72e3ffb04f0a3dd29dd8198
Andrea Chello <42430017+achello94@users.noreply.github.com>
2019-11-30 18:10:19 +0000

    Create plot2.R

---
+++ ExploratoryData/Project1/plot2.R
@@ -0,0 +1,18 @@
+# Plot 2: Global Active Power Scatterplot
+
+library(data.table)
+library(lubridate)
+
+data <- fread("household_power_consumption.txt", sep = ";", na.strings = "?")
+
+subsetted_data <- data[data$Date %in% c("1/2/2007", "1/2/2007"), ]
+
+glob <- sub_data$Global_active_power
+dates <- as.Date(sub_data$Date, format = "%d/%m/%Y")
+datetime <- as.POSIXct(paste(dates, sub_data$Time))
+
+
+
+png(filename = "plot2.png", width = 480, height = 480)
+with(sub_data, plot(glob ~ datetime, type = "l", xlab = "", ylab = "Global Active Power (kilowatts"))
+dev.off()

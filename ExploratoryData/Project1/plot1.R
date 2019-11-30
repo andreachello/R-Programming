70034f7f91db75558a526d6b2f1a0388c2a6bee2
Andrea Chello <42430017+achello94@users.noreply.github.com>
2019-11-30 18:09:43 +0000

    Create plot1.R

---
+++ ExploratoryData/Project1/plot1.R
@@ -0,0 +1,14 @@
+
+library(data.table)
+library(lubridate)
+
+# Global data table
+data <- fread("household_power_consumption.txt", sep = ";", na.strings = "?")
+
+subsetted_data <- data[data$Date %in% c("1/2/2007", "1/2/2007"), ]
+
+# Plot 1: Global Active Power Histogram
+
+png(filename = "plot1.png", width = 480, height = 480)
+hist(as.numeric(subsetted_data$Global_active_power), col = ' red', xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
